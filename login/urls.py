e8133ea688482c5d19186104943cae36afb93d64
muzammilbinmarsuhen <j7958962@gmail.com>
2023-12-28 14:21:12 +0700

    ada perubahan untuk mengatasi pesan reverse

--- login/urls.py
+++ login/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . views import login_view
+from .views import login_view
 
 urlpatterns = [
-    path('', login_view, name='login_view'),
+    path('login/', login_view, name='login'),
 ]
